b1ece782c36addcd9dd045bcf8d8ca976c96c6cd
AishuPandian <aishuap@gmail.com>
2019-07-15 16:14:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,21 @@ const pool = new Pool({
 //const pool = new pg.Pool(pool)
 //pg.connect('postgres://postgres:password@localhost:5432/practicedocker');
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
-server.get('/', (req, res) => res.status(200).send('hello'));
+//server.get('/', (req, res) => res.status(200).send('hello'));
+
+server.get('/', (req, res, next) => {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        }
+        client.query('SELECT * FROM users', function (err, result) {
+             done();
+             if (err) {
+                 console.log(err);
+                 res.status(400).send(err);
+             }
+             res.status(200).send(result.rows);
+        })
+    })
+ });
+
